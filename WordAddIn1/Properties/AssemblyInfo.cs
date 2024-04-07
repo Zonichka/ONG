2cfb9177e16cefe30fad615747bdfb1988b7cb7a
Yan <73839771+YanMuhamadeev@users.noreply.github.com>
2024-04-07 20:37:54 +0500

    Add files via upload

---
+++ WordAddIn1/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Управление общими сведениями о сборке осуществляется посредством следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("WordAddIn1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WordAddIn1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если необходимо обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ea7ad444-ce96-4e1e-a1f4-cdf3227a70a3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ WordAddIn1/ThisAddIn.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using System.Windows.Forms;
+
+namespace WordAddIn1
+{
+    public partial class ThisAddIn
+    {
+
+        private Office.CommandBarPopup contextMenuPopup;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            // Получаем объект CommandBar контекстного меню
+            Office.CommandBar contextMenu = ((Office.CommandBars)this.Application.CommandBars)["Text"];
+
+            // Создаем новый пункт подменю в контекстном меню
+            contextMenuPopup = (Office.CommandBarPopup)contextMenu.Controls.Add(
+                Office.MsoControlType.msoControlPopup, Type.Missing, Type.Missing, Type.Missing, true);
+
+            // Настройка свойств пункта подменю
+            contextMenuPopup.Caption = "Моя надстройка";
+
+            // Создаем элементы подменю
+            Office.CommandBarButton menuItem1 = (Office.CommandBarButton)contextMenuPopup.Controls.Add(
+                Office.MsoControlType.msoControlButton, Type.Missing, Type.Missing, Type.Missing, true);
+            menuItem1.Caption = "Дополнить текст";
+            menuItem1.Click += new Office._CommandBarButtonEvents_ClickEventHandler(menuItem1_Click);
+
+            Office.CommandBarButton menuItem2 = (Office.CommandBarButton)contextMenuPopup.Controls.Add(
+                Office.MsoControlType.msoControlButton, Type.Missing, Type.Missing, Type.Missing, true);
+            menuItem2.Caption = "Тест2";
+            menuItem2.Click += new Office._CommandBarButtonEvents_ClickEventHandler(menuItem2_Click);
+        }
+
+        private void menuItem1_Click(Office.CommandBarButton Ctrl, ref bool CancelDefault)
+        {
+            // Получаем объект Range текущего выделения в документе Word
+            Word.Range range = this.Application.Selection.Range;
+
+            // Добавляем к текущему местоположению текст
+            string additionalText = "Вставка";
+            range.InsertAfter(" " + additionalText);
+        }
+
+        private void menuItem2_Click(Office.CommandBarButton Ctrl, ref bool CancelDefault)
+        {
+            // Действия, выполняемые при клике на элемент подменю "Тест2"
+            MessageBox.Show("Выбрано: Тест2");
+        }
+
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
